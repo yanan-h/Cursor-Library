86a0987edcccd8badfcc449caf22e7f44745084a
yanan-h <yananhou@cca.edu>
2023-11-20 15:58:53 -0800

    change to english

--- script.js
+++ script.js
@@ -19,52 +19,47 @@ const userName = localStorage.getItem('userName');
 
 // 获取所有拥有 'box' 类的元素
 let boxes = document.querySelectorAll(".box");
-let isMovable = false; // 控制所有元素是否可以移动的标志
+let isMovable = false; 
 
-// 获取控制开关元素
+
 let move = document.getElementById("move");
 
-// 设置双击事件处理器来切换移动状态
+
 move.ondblclick = function() {
   isMovable = !isMovable;
-  move.textContent = isMovable ? "双击以禁用移动" : "双击以启用移动";
+  move.textContent = isMovable ? "doubleClickNoMove" : "doubleClickToMove";
 };
 
-// 为整个文档添加双击事件处理器
+
 document.ondblclick = function(event) {
-  // 检查双击的是否是控制开关本身
+ 
   if (event.target !== move) {
     isMovable = false;
-    move.textContent = "双击以启用移动";
+    move.textContent = "doubleClickToMove";
   }
 };
 
 boxes.forEach(function(box) {
   box.onmousedown = function(event) {
-    if (!isMovable) return; // 如果不在可移动状态，不执行任何操作
-
-    // 计算鼠标相对于元素的初始位置
+    if (!isMovable) return; 
+ 
     let disx = event.clientX - box.offsetLeft;
     let disy = event.clientY - box.offsetTop;
 
-    // 添加mousemove事件到document
     document.onmousemove = function(event) {
       box.style.left = event.clientX - disx + 'px';
       box.style.top = event.clientY - disy + 'px';
     };
-
-    // 添加mouseup事件到document
+   
     document.onmouseup = function() {
       document.onmousemove = null;
       document.onmouseup = null;
       return false;
     };
 
-    // 防止默认事件
     return false;
   };
 
-  // 防止 Firefox 上的默认拖动行为
   box.ondragstart = function() {
     return false;
   };
